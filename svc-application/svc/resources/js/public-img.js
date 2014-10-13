c1a5a394acc8ffe74b4e14eaf749527927c03dae
Peter O'Toole <peter.otoole14@gmail.com>
2014-10-13 22:45:50 +0100

    Set up project files, added formatter

---
+++ svc-application/svc/resources/js/public-img.js
@@ -0,0 +1,14 @@
+$ ( document ).ready ( function( ) {
+
+	$ ( "#banner_logo-img" ).click ( function( ) {
+
+		location = "/";
+	} );
+	$ ( "#banner_title" ).click ( function( ) {
+
+		location = "/";
+	} );
+
+	var imgData = $ ( "#img-data" ).html ( );
+	$ ( "#public-share-image" ).html ( "<img id='public-share-image-img' src='" + imgData + "' >" );
+} );
