6707349e58b0f290fbdde001504d65e8fdda3be8
Peter O'Toole <peter.otoole14@gmail.com>
2016-03-13 22:16:35 +0000

    Adding webstorm settings for project

--- svc-application/main/lib/common.js
+++ svc-application/main/lib/common.js
@@ -1,32 +1,32 @@
 /**
- * Social Visual Communication, Copyright © Peter O'Toole All Rights Reserved
- * 
+ * Social Visual Communication, Copyright Peter O'Toole All Rights Reserved
+ *
  * @author Peter O'Toole peterjotoole@outlook.com
- * 
+ *
  * @version 0.0.1
  * @since 10/14/2014
- * 
- * 
+ *
+ *
  * common.js - Contains constants for the application
  */
+"use strict"
 
 exports = {
-
-    HTTP_PORT: 80,
-    SOCKET_IO_PORT: 8000,
-    WEB_ADDRESS: "127.0.0.1",
-    DATA_BASE: "",
-    DATA_BASE_SERVER: "127.0.0.1",
-    DATA_BASE_USER: "",
-    DATA_BASE_PASSWORD: "",
-    DATA_BASE_IDEL: 1000,
-    DATA_BASE_MAX: 20,
-    DATA_BASE_MIN: 0,
-    MAILER_HOST: "smtp.gmail.com",
-    MAILER_FROM: "no-reply@svc.com",
-    MAILER_PORT: 465,
-    MAILER_PROTOCOL: "SMTP",
-    MAILER_USERNAME: "svc.no.reply@gmail.com",
-    MAILER_PASSWORD: "willowstick",
-    EXPRESS_SESSION_SECRET: ""
-};
+	HTTP_PORT:              80,
+	SOCKET_IO_PORT:         8000,
+	WEB_ADDRESS:            "127.0.0.1",
+	DATA_BASE:              "",
+	DATA_BASE_SERVER:       "127.0.0.1",
+	DATA_BASE_USER:         "",
+	DATA_BASE_PASSWORD:     "",
+	DATA_BASE_IDEL:         1000,
+	DATA_BASE_MAX:          20,
+	DATA_BASE_MIN:          0,
+	MAILER_HOST:            "smtp.gmail.com",
+	MAILER_FROM:            "no-reply@svc.com",
+	MAILER_PORT:            465,
+	MAILER_PROTOCOL:        "SMTP",
+	MAILER_USERNAME:        "svc.no.reply@gmail.com",
+	MAILER_PASSWORD:        "willowstick",
+	EXPRESS_SESSION_SECRET: ""
+}
